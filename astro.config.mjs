8676e43e97689b211a2ea865ad6a08d36e37bb28
Brian Exequiel Putallaz <brianexequielputallaz@gmail.com>
2025-02-09 15:32:39 -0300

    feat: :rocket: Deploy github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://github.com/BrianPutallazExequiel/course-astro.git',
+    base: 'course-astro',
+});
