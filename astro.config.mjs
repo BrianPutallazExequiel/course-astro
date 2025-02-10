6e94232050d6fffac5fd081e83b00f54bef3851a
Brian Exequiel Putallaz <brianexequielputallaz@gmail.com>
2025-02-10 00:18:13 -0300

    feat: :sparkles: Class S2/L11- View Transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: 'https://github.com/BrianPutallazExequiel/course-astro.git',
-    base: 'course-astro',
+    // base: 'course-astro',
 });
